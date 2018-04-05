63a5bea0c2bc055919d02088bf239b307923f656
vior <v.orlov@querix.com>
2018-04-05 12:13:11 +0300

    MAIA-99 Maia demo: Add credits to Maia Associates

---
+++ querix/js/maia-footnote.js
@@ -0,0 +1,21 @@
+var footnoteHtml = 'MaiaERP is a Community Supported, Modular, Multi-Company, Multi-Language, '+
+'Multi-Currency, Fully Analytical ERP. Curtsey of <a href="http://www.maiaerp.org" target="_blank">Maia Associates</a>. '+
+'To access the source code, please contact <a href="mailto:ericv@maiaerp.org">Eric Vercelletto</a>';
+
+var addFootnote = function addFootnote() {
+  var topdoc = window.top.document;
+  if (!topdoc.querySelector('#qx-maia-footnote')) {
+    var footnote = topdoc.createElement('div');
+    footnote.setAttribute('id', 'qx-maia-footnote');
+    footnote.innerHTML = footnoteHtml;
+    topdoc.body.appendChild(footnote);
+    window.top.requestAnimationFrame(function(){
+      var up = window.top.querix.updatePager;
+      if (up) {
+        up();
+      }
+    })
+  }
+}
+
+addFootnote();
